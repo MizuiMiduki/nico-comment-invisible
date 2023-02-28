59c09a7b5aaa6fd19d676f56ec38e8a1908cd516
MizuiMiduki <105696149+MizuiMiduki@users.noreply.github.com>
2023-02-28 11:46:56 +0900

    R3.2

--- main.js
+++ main.js
@@ -43,6 +43,9 @@ $(function () {
         $('.MainContainer-playerPanel').css('display', 'none');
         //上記によりプレイヤーが引き伸ばされるため、動画を中央に寄せる
         $('.VideoContainer').css('margin', '0 auto');
+        //動画サイズ拡大
+        $('.VideoContainer').css('padding-top', '42.1875%');
+        $('.VideoContainer').css('width', '100%');
         //コメ欄表示ボタンを設置
         $('.CommentOnOffButton').before('<button id="comments-enable-ex" class="ActionButton ControllerButton comments-ex-class" data-title="コメント欄を表示する(拡張機能)"><div class="ControllerButton-inner"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"><path d="M5.833 11.708q-.354 0-.614-.26-.261-.26-.261-.615 0-.354.261-.614.26-.261.614-.261.355 0 .615.261.26.26.26.614 0 .355-.26.615t-.615.26Zm11.563 3.084-1.479-1.459h.666V3.417H6l-1.75-1.75h12.417q.687 0 1.177.489.489.49.489 1.177v10q0 .542-.281.917t-.656.542ZM9.354 6.75 8.333 5.729V5H15v1.75Zm2.417 2.417-1.75-1.75H15v1.75Zm5.312 10.25L12.625 15H5l-3.333 3.333V4L.583 2.917l1.229-1.229 16.5 16.5ZM3.417 5.75v8.354l.854-.854h6.646Zm7.541 2.625ZM7.167 9.5Zm-1.334-.292q-.354 0-.614-.26-.261-.26-.261-.615 0-.354.261-.614.26-.261.614-.261.355 0 .615.261.26.26.26.614 0 .355-.26.615t-.615.26Z"/></svg></div><buton>');
     });
@@ -92,6 +95,9 @@ $(function () {
                 $('.MainContainer-playerPanel').css('display', 'none');
                 //動画を中央寄せ
                 $('.VideoContainer').css('margin', '0 auto');
+                //動画サイズ拡大
+                $('.VideoContainer').css('padding-top', '42.1875%');
+                $('.VideoContainer').css('width', '100%');
                 //コメ欄非表示ボタンを削除
                 $('#comments-disable-ex').remove();
                 $('#comments-enable-ex').remove();
